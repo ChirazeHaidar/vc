fbad3d6911189e363c21b5194ed71d66402462b6
Mohamed Faour <mouhammad.faour@isae.edu.lb>
2017-09-29 01:13:49 +0300

    Merge origin/master
    Conflicts:
    	StockManagement Solution/StockManagement.Services/nbproject/private/private.properties
    	StockManagement Solution/StockManagement.Web/nbproject/private/private.properties

---
+++ StockManagement Solution/StockManagement.Web/src/java/StockManamgement/Web/Utilities/Configuration.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StockManamgement.Web.Utilities;
+
+/**
+ *
+ * @author mfaour
+ */
+public class Configuration {
+   public static String ServiceURL = "http://35.194.131.184:8090/StockManagement.Services/webresources"; 
+}

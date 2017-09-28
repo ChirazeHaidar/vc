3f239f21a48eaa1586726fd6f2b05274660f0ef4
MikeRmaily <mike.rmaily@isae.edu.lb>
2017-09-28 16:47:04 +0300

    Products orders Chart jsf and bean

--- StockManagement Solution/StockManagement.Web/src/java/StockManamgement/Web/Beans/ChartBean.java
+++ StockManagement Solution/StockManagement.Web/src/java/StockManamgement/Web/Beans/ChartBean.java
@@ -8,6 +8,7 @@ package StockManamgement.Web.Beans;
 import StockManagement.ObjectModel.ValueObject.Chart;
 import StockManagement.Services.ChartClient;
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -19,7 +20,6 @@ import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.LineChartModel;
-import org.primefaces.model.chart.LineChartSeries;
 /**
  *
  * @author MikeRmaily
@@ -30,9 +30,18 @@ public class ChartBean implements Serializable {
     private List<Chart> ProductsOrders;
     private LineChartModel animatedModel1;
    private BarChartModel animatedModel2;
+     private BarChartModel animatedModel3;
  
    public List<Chart> getProductsOrders() {
         return ProductsOrders;
+    }
+
+    public BarChartModel getAnimatedModel3() {
+        return animatedModel3;
+    }
+
+    public void setAnimatedModel3(BarChartModel animatedModel3) {
+        this.animatedModel3 = animatedModel3;
     }
 
     public void setProductsOrders(List<Chart> ProductsOrders) {
@@ -54,7 +63,7 @@ public class ChartBean implements Serializable {
     @PostConstruct
     public void init() {
           refreshData();
-           createAnimatedModels();
+          
     }
 
     private void refreshData() {
@@ -63,7 +72,7 @@ public class ChartBean implements Serializable {
         ProductsOrders = service.GetProductsOrders(gType);
         
         if (! ProductsOrders.isEmpty()){
-          
+           createAnimatedModels();
           
         }
 }
@@ -77,73 +86,30 @@ public class ChartBean implements Serializable {
     }
  
     private void createAnimatedModels() {
-        animatedModel1 = initLinearModel();
-        animatedModel1.setTitle("Line Chart");
-        animatedModel1.setAnimate(true);
-        animatedModel1.setLegendPosition("se");
-        Axis yAxis = animatedModel1.getAxis(AxisType.Y);
-        yAxis.setMin(0);
-        yAxis.setMax(10);
-         
-        animatedModel2 = initBarModel();
-        animatedModel2.setTitle("Bar Charts");
-        animatedModel2.setAnimate(true);
+
+       animatedModel2 = initBarModel();
+       animatedModel2.setTitle("Product Orders");
+      animatedModel2.setAnimate(true);
         animatedModel2.setLegendPosition("ne");
-        yAxis = animatedModel2.getAxis(AxisType.Y);
+         Axis  yAxis = animatedModel2.getAxis(AxisType.Y);
         yAxis.setMin(0);
-        yAxis.setMax(200);
+       yAxis.setMax(10);
+   
     }
      
     private BarChartModel initBarModel() {
         BarChartModel model = new BarChartModel();
  
-        ChartSeries boys = new ChartSeries();
-        boys.setLabel("Boys");
-        boys.set("2004", 120);
-        boys.set("2005", 100);
-        boys.set("2006", 44);
-        boys.set("2007", 150);
-        boys.set("2008", 25);
- 
-        ChartSeries girls = new ChartSeries();
-        girls.setLabel("Girls");
-        girls.set("2004", 52);
-        girls.set("2005", 60);
-        girls.set("2006", 110);
-        girls.set("2007", 135);
-        girls.set("2008", 120);
- 
-        model.addSeries(boys);
-        model.addSeries(girls);
-         
-        return model;
-    }
-     
-    private LineChartModel initLinearModel() {
-        LineChartModel model = new LineChartModel();
- 
-        LineChartSeries series1 = new LineChartSeries();
-        series1.setLabel("Series 1");
- 
-        series1.set(1, 2);
-        series1.set(2, 1);
-        series1.set(3, 3);
-        series1.set(4, 6);
-        series1.set(5, 8);
- 
-        LineChartSeries series2 = new LineChartSeries();
-        series2.setLabel("Series 2");
- 
-        series2.set(1, 6);
-        series2.set(2, 3);
-        series2.set(3, 2);
-        series2.set(4, 7);
-        series2.set(5, 9);
- 
-        model.addSeries(series1);
-        model.addSeries(series2);
-         
+        ChartSeries Orders = new ChartSeries();
+        Orders.setLabel("Orders");
+        for (Iterator<Chart> it = ProductsOrders.iterator(); it.hasNext();) {
+            Chart Order = it.next();
+            Orders.set(Order.getPrName(), Order.getOrders());
+        }
+        model.addSeries(Orders);         
         return model;
     }
     
+
+
 }
